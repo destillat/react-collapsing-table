9ffc467b7c2f07f79182d89f8a293203328acc9b
Paul Darragh <pdarragh45@massmutual.com>
2017-12-01 14:18:37 -0500

    bumped version

--- src/__tests__/utils/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = ({ row, accessor, CustomFunction }) => {
-  return <button onClick={ CustomFunction }>{ row[accessor] }</button>
-};
-
-export default Button;
