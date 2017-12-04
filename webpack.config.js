614f9bca5a495157bb4df8e31ca675faad0e89be
Paul Darragh <pdarragh45@massmutual.com>
2017-12-04 09:24:32 -0500

    trying to fix webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,27 @@
 var path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, 'src', 'assets', 'styles'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
         ]
-      }
-    ]
-  },
-  externals: {
-    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
-  plugins: [
-    new ExtractTextPlugin("react-table.css"),
-  ]
+    },
+    externals: {
+        'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    }
 };
