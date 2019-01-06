829f7cb2a2939ecb59944c37f2d4e140a199d117
Adrien Boutigny <adrien.boutigny@gmail.com>
2019-01-06 23:50:13 +0100

    add a story to demonstrate dynamic columns

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../src/assets/styles/global.css';
 
 import { storiesOf } from '@storybook/react';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
 
 import ReactCollapsingTable from '../src/index';
 import {
@@ -20,6 +21,7 @@ import {
 
 
 storiesOf('React Collapsing Table', module)
+    .addDecorator(withKnobs)
     .add('Basic Table', () => <ReactCollapsingTable {...basicTableProps} />)
     .add('Turn Pagination and Search On', () => <ReactCollapsingTable {...basicTablePropsPaginationAndSearchShow } />)
     .add('Set Column and Direction displayed on load', () => <ReactCollapsingTable {...sortColumnAndDirectionProps} />)
@@ -30,4 +32,8 @@ storiesOf('React Collapsing Table', module)
     .add('Custom Text Input Pagination', () => <ReactCollapsingTable {...customPaginationComponent} />)
     .add('Custom Icons for the open/close row and de/ascending icon', () => <ReactCollapsingTable {...customIconProps} />)
     .add('Columns with sort feature disabled', () => <ReactCollapsingTable {...unsorted} />)
-    .add('Custom theme, no applied styles', () => <ReactCollapsingTable {...differentTheme} />);
+    .add('Custom theme, no applied styles', () => <ReactCollapsingTable {...differentTheme} />)
+    .add('Dynamic table columns', () => {
+        const dynamicColumns = basicTableProps.columns.filter(column => boolean(column.label, true))
+        return <ReactCollapsingTable {...basicTableProps} columns={dynamicColumns} />
+    });
